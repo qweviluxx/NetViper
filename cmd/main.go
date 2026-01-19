6c63cf9dce540a6de21f30123136414b8552dbe3
qweviluxx <kovalfw@gmail.com>
2026-01-19 21:49:16 +0300

    Scanner done

--- cmd/main.go
+++ cmd/main.go
@@ -4,17 +4,18 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/qweviluxx/GopherScanner.git/internal"
+	. "github.com/qweviluxx/GopherScanner.git/internal"
 )
 
 func main() {
-	s := internal.NewScanner("tcp")
+	s := NewScanner("tcp")
 	conn, err := s.ScanPort("scanme.nmap.org", 80)
 	fmt.Println(conn)
 	fmt.Println(err)
 
 	start := time.Now()
-	s.ScanRange("scanme.nmap.org", 20, 100)
+	ports := s.ScanRange("scanme.nmap.org", 20, 1000)
 	duration := time.Since(start)
 	fmt.Println(duration)
+	fmt.Println(ports)
 }
--- internal/scanner.go
+++ internal/scanner.go
@@ -8,18 +8,18 @@ import (
 )
 
 type Scanner struct {
-	Protocol string
-	timeout  time.Duration
+	Protocol    string
+	timeout     time.Duration
+	workerCount int
 }
 
 func NewScanner(protocol string) *Scanner {
 	return &Scanner{
-		Protocol: protocol,
-		timeout:  1 * time.Second,
+		Protocol:    protocol,
+		timeout:     500 * time.Millisecond,
+		workerCount: 100,
 	}
 }
-
-var timeout time.Duration = 500 * time.Millisecond
 
 func (s *Scanner) ScanPort(hostname string, port int) (bool, error) {
 	address := hostname + fmt.Sprintf(":%d", port)
@@ -32,20 +32,51 @@ func (s *Scanner) ScanPort(hostname string, port int) (bool, error) {
 	return true, nil
 }
 
-func (s *Scanner) ScanRange(hostname string, startPort, endPort int) {
+func (s *Scanner) ScanRange(hostname string, startPort, endPort int) []int {
+
+	size := endPort - startPort + 1
+	openedPorts := []int{}
+
+	ports := make(chan int, size)
+	result := make(chan int)
+
 	var wg sync.WaitGroup
-	for i := startPort; i <= endPort; i++ {
+
+	for i := 0; i < s.workerCount; i++ {
 		wg.Add(1)
-
-		go func(p int) {
-			defer wg.Done()
-			ok, _ := s.ScanPort(hostname, p)
-			if ok {
-				fmt.Println("Port", p, "is open")
-			}
-		}(i)
-
+		go s.worker(&wg, hostname, ports, result)
 	}
 
-	wg.Wait()
+	go func() {
+		for j := startPort; j <= endPort; j++ {
+			ports <- j
+		}
+		close(ports)
+	}()
+
+	go func() {
+		wg.Wait()
+		close(result)
+	}()
+
+	for p := range result {
+		res := p
+		if res != 0 {
+			openedPorts = append(openedPorts, res)
+		}
+	}
+
+	return openedPorts
+}
+
+func (s *Scanner) worker(wg *sync.WaitGroup, hostname string, ports chan int, result chan int) {
+	defer wg.Done()
+	for i := range ports {
+		ok, _ := s.ScanPort(hostname, i)
+		if ok {
+			result <- i
+		} else {
+			result <- 0
+		}
+	}
 }
