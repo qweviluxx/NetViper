41aeb2e970db15eba250e3ad844dcad52401dbdd
qweviluxx <kovalfw@gmail.com>
2026-01-21 13:36:58 +0300

    Scanner reading params of request

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,13 +3,40 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
-	. "github.com/qweviluxx/GopherScanner.git/internal"
+	"github.com/qweviluxx/GopherScanner.git/internal"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	scanner := NewScanner("tcp")
-	ports := scanner.ScanRange("scanme.nmap.org", 20, 1000)
+	scanner := internal.NewScanner("tcp")
+
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method is not supported", http.StatusMethodNotAllowed)
+		return
+	}
+
+	params := r.URL.Query()
+	hostname := params.Get("hostname")
+	startPort, err := strconv.Atoi(params.Get("startport"))
+	if err != nil {
+		http.Error(w, "Parsing param error:", http.StatusBadRequest)
+		return
+	}
+
+	endPort, err := strconv.Atoi(params.Get("endport"))
+
+	if err != nil {
+		http.Error(w, "Parsing param error:", http.StatusBadRequest)
+		return
+	}
+
+	if hostname == "" {
+		http.Error(w, "Parsing param error:", http.StatusBadRequest)
+		return
+	}
+
+	ports := scanner.ScanRange(hostname, startPort, endPort)
 	fmt.Fprintf(w, "opened ports:%v", ports)
 
 }
